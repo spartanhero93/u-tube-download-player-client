e97bc56fcf7114bd29b193c8a6bd390152204658
Luis Lopez <name.luislopez@gmail.com>
2019-05-16 16:22:20 -0400

    youtube-api-search installed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import searchYoutube from 'youtube-api-v3-search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: {},
+    text: ''
+  }
+
+  handelText = ({ target }) =>
+    this.setState({ text: target.value })
+
+  search = async () => {
+    try {
+      const data = await searchYoutube(
+        'API_KEY',
+        this.state.text
+      )
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Welcome to Youtube App</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
