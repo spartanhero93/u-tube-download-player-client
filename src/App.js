c37f4f1db27e5ae8982e2e82217026114dd5cc9b
Luis Lopez <name.luislopez@gmail.com>
2019-05-17 15:13:03 -0400

    retrival of video data added

--- src/App.js
+++ src/App.js
@@ -12,20 +12,46 @@ class App extends React.Component {
 
   search = async () => {
     try {
-      const data = await searchYoutube(
-        'API_KEY',
-        this.state.text
-      )
-      console.log(data)
+      const data = await searchYoutube('API_KEY', {
+        q: this.state.text
+      })
+      this.setState({
+        data: data.items
+      })
     } catch (error) {
       console.error(error)
     }
   }
 
   render () {
+    const { data } = this.state
+    console.log(this.state.data)
     return (
       <div>
         <h1>Welcome to Youtube App</h1>
+        <button onClick={() => this.search()}>Fetch</button>
+        <input
+          onChange={e => this.handelText(e)}
+          type='text'
+        />
+        {data.length ? (
+          data.map(i => (
+            <div
+              style={{ padding: '1rem 0' }}
+              id={i.id.videoId}
+            >
+              <h2>{i.snippet.title}</h2>
+              <h5>{i.snippet.description}</h5>
+              <div>{i.id.videoId}</div>
+              <img
+                src={i.snippet.thumbnails.default.url}
+                alt={i.snippet.title}
+              />
+            </div>
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     )
   }
