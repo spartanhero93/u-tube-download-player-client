5ade8641eab6947f7da93471c11bb351edec8bcf
Luis Lopez <name.luislopez@gmail.com>
2019-05-29 16:11:58 -0400

    fixed api_key issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import searchYoutube from 'youtube-api-v3-search'
 import { RenderItems } from './components/RenderItems'
-
-const SERVER_URL = 'http://localhost:3001'
+import { KEY } from './key'
+// const SERVER_URL = 'http://localhost:3001'
 
 function App () {
   const [data, handleData] = useState({})
@@ -10,7 +10,7 @@ function App () {
 
   const requestData = async term => {
     try {
-      const data = await searchYoutube('API_KEY', {
+      const data = await searchYoutube(KEY, {
         q: term
       })
       handleData(data)
@@ -33,78 +33,5 @@ function App () {
     </div>
   )
 }
-
-// class App extends React.Component {
-//   state = {
-//     data: {},
-//     text: ''
-//   }
-
-//   handelText = ({ target }) =>
-//     this.setState({ text: target.value })
-
-//   search = async () => {
-//     try {
-//       const data = await searchYoutube('API_KEY', {
-//         q: this.state.text
-//       })
-//       this.setState({
-//         data: data.items
-//       })
-//     } catch (error) {
-//       console.error(error)
-//     }
-//   }
-
-//   handleClickDownload = async vidId => {
-//     try {
-//       const res = await axios.post(
-//         `${SERVER_URL}/api/${vidId}`
-//       )
-//       console.log(res)
-//     } catch (error) {
-//       console.error(error)
-//     }
-//   }
-
-//   render () {
-//     const { data } = this.state
-//     console.log(this.state.data)
-//     return (
-//       <div>
-//         <h1>Welcome to Youtube App</h1>
-//         <button onClick={() => this.search()}>Fetch</button>
-//         <input
-//           onChange={e => this.handelText(e)}
-//           type='text'
-//         />
-//         {data.length ? (
-//           data.map(i => (
-//             <div
-//               style={{ padding: '1rem 0' }}
-//               id={i.id.videoId}
-//             >
-//               <h2>{i.snippet.title}</h2>
-//               <h5>{i.snippet.description}</h5>
-//               <button
-//                 onClick={() =>
-//                   this.handleClickDownload(i.id.videoId)
-//                 }
-//               >
-//                 {i.id.videoId}
-//               </button>
-//               <img
-//                 src={i.snippet.thumbnails.default.url}
-//                 alt={i.snippet.title}
-//               />
-//             </div>
-//           ))
-//         ) : (
-//           <div>Loading...</div>
-//         )}
-//       </div>
-//     )
-//   }
-// }
 
 export default App
